3797bccc5befd19f6a7100d24eb1a0c367aa5616
Lazar Dondo <ldondo@s4hc.com>
2022-04-14 09:10:36 +0200

    Add closeAuction handler

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,18 @@
-async function processAuctions(event, context){
+import createError from 'http-errors';
+import {getEndedAuctions} from '../lib/getEndedAuctions';
+import {closeAuction} from '../lib/closeAuction';
 
+async function processAuctions(event, context){
+    try{
+    const auctionsToClose = await getEndedAuctions();
+    const closedPromises = auctionsToClose.map(auction=>closeAuction(auction));
+    await Promise.all(closedPromises);
+    return {closed: closedPromises.length};
+    }
+    catch(error){
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
 }
 
 export const handler = processAuctions
